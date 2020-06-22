b26200fce98d0cc6d87e3a4a0703c88c334722cf
yeon94 <hjy9946@naver.com>
2020-06-22 23:41:10 +0900

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = 4000;
+const PORT = 5000;
 
 function handleListening() {
 	console.log(`Listening on: http://localhost:${PORT}`);
